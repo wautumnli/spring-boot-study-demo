1931683caf2292115a8bbcd3c4e83148faeb9f9d
万秋里 <wanqiuli@jd.com>
2021-02-20 17:38:41 +0800

    Kaptcha 验证码

---
+++ spring-boot-demo/src/main/java/com/ql/demo/verificode/config/KaptchaController.java
@@ -0,0 +1,39 @@
+package com.ql.demo.verificode.config;
+
+import com.google.code.kaptcha.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Author： wanqiuli
+ * DateTime: 2021/2/20 17:28
+ */
+@Controller
+public class KaptchaController {
+
+    @Autowired
+    private Producer producer;
+
+    @RequestMapping("/kaptcha.jpg")
+    public void captcha(HttpServletResponse response, HttpServletRequest request) throws IOException {
+        response.setHeader("Cache-Control", "no-store, no-cache");
+        response.setContentType("image/jpeg");
+
+        String text = producer.createText();
+        BufferedImage image = producer.createImage(text);
+        // 保存到session中或者redis中
+        HttpSession session = request.getSession();
+        session.setAttribute("kaptcha", text);
+        ServletOutputStream outputStream = response.getOutputStream();
+        ImageIO.write(image, "jpg", outputStream);
+    }
+}
