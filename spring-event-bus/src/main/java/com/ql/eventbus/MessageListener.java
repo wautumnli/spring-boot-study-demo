e53d7c91b602eb3194297ddd86ef9f34fae52861
万秋里 <wanqiuli@jd.com>
2021-02-23 18:14:05 +0800

    eventBus simple demo

--- spring-event-bus/src/main/java/com/ql/eventbus/MessageListener.java
+++ spring-event-bus/src/main/java/com/ql/eventbus/MessageListener.java
@@ -1,5 +1,6 @@
 package com.ql.eventbus;
 
+import com.google.common.eventbus.AllowConcurrentEvents;
 import com.google.common.eventbus.Subscribe;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,12 +14,19 @@ public class MessageListener {
     private static int eventHandled;
 
     @Subscribe
+    @AllowConcurrentEvents
     public void handleEvent(MessageEvent event) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         log.info("do event info [" + event.getMessage() + "]");
         eventHandled++;
     }
 
     @Subscribe
+    @AllowConcurrentEvents
     public void eventWarn(MessageEvent event) {
         log.warn("do event warn [" + event.getMessage() + "]");
     }
--- spring-event-bus/src/main/java/com/ql/eventbus/MessageMain.java
+++ spring-event-bus/src/main/java/com/ql/eventbus/MessageMain.java
@@ -1,7 +1,13 @@
 package com.ql.eventbus;
 
+import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Author： wanqiuli
@@ -11,8 +17,24 @@ import lombok.extern.slf4j.Slf4j;
 public class MessageMain {
 
     public static void main(String[] args) {
+        asynEvnetBus();
+    }
+
+    // 同步
+    public static void syncEventBus() {
         MessageListener listener = new MessageListener();
         final EventBus eventBus = new EventBus();
+        eventBus.register(listener);
+        eventBus.post(new MessageEvent("message event test"));
+        eventBus.post(new MessageEvent("message event test again"));
+        log.info("handle event count: {}", listener.getEventHandled());
+    }
+
+    // 异步
+    public static void asynEvnetBus() {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+        MessageListener listener = new MessageListener();
+        final AsyncEventBus eventBus = new AsyncEventBus("messageEventAsync", threadPoolExecutor);
         eventBus.register(listener);
         eventBus.post(new MessageEvent("message event test"));
         eventBus.post(new MessageEvent("message event test again"));
